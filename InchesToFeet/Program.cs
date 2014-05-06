451eff26841594b3d9ee00f0cabeecfa19fd5fc4
RyanClarker <clarke.ryan24@gmail.com>
2014-05-06 10:09:49 -0400

    new

---
+++ InchesToFeet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToFeet
+{
+    class InchesToFeet
+    {
+        public static void ToFeet(int inches)
+        {
+            Console.WriteLine("{0} feet and {1} inches", inches / 12, inches % 12);
+        }
+
+        public static void Yards(int inches)
+        {
+            Console.WriteLine("{0} yards and {1} feet and {2} inches", inches / 12 / 3, (inches / 12 / 3) - (inches / 12), inches % 12);
+ 
+        }
+        public static void Main()
+        {
+            int inches = 79;
+            ToFeet(inches);
+            Yards(inches);
+        }
+    }
+}
